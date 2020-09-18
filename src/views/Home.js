3ee5fea2a50ddd8a2fa2aeb5f787a550db4985d8
Yuanita Retno <48400221+yrfp307@users.noreply.github.com>
2020-09-18 17:11:56 +0800

    2nd comit

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
 import styled from 'styled-components';
 
 const Styles = styled.div`
 .card {
   font-size: 1.5em;
   position: center;
-  margin: 200px;
+  margin-top: 50px;
+  margin-left: 100px;
+  margin-right: 100px;
+  max-width: 100vw;
 }`;
 
 export default function Home () {
   return (
     <div className="Home">
     <Styles>
-    <Card>
+    <Card >
     <Card.Header>Quote of the day!</Card.Header>
     <Card.Body>
-    <blockquote className="blockquote mb-10">
+    <blockquote className="blockquote mb-0">
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
         erat a ante.
